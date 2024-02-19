5b04e425f82e0b95231a93beeb31233fc3852e32
Kyle Gorman <kylebgorman@gmail.com>
2024-02-19 15:41:30 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+The queries and counts are:
+
+1.  `FRAG`: 18
+2.  `CD`: 647
+3.  `NP < DT`: 1,128
+4.  `NP < (DT < a)`: 274
+5.  `NP < (NP $ CC $ NP)`: 128
+6.  `VP << NP`: 1,535
+7.  `VP < NP`: 548
+8.  `VP !< NP`: 1,288
+9.  `VP < (NP $ NP)`: 7
+10. `VP < (NP < PP)`: 139
+
+See \[`hw4.py`\] for the code. Sample output:
+
+    $ ./hw4.py 
+    1:     18
+    2:    647
+    3:  1,128
+    4:    274
+    5:    128
+    6.  1,535
+    7.    548
+    8.  1,288
+    9.      7
+    10.   139
---
+++ hw4.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""Tree queries for HW4."""
+
+from typing import List
+
+from nltk import tgrep, tree
+
+
+DATA = "data/wsj.parse"
+
+
+def count_nodes(query: str, forest: List[tree.ParentedTree]) -> int:
+    """Computes the number of nodes which match a TGrep query.
+
+    This function runs a tgrep_nodes query, catches the resulting generator,
+    and returns the number of matching nodes.
+
+    Args:
+        query: a tgrep query string
+        forest: a list of one or more `ParentedTree`s to be queried
+
+    Returns:
+        the number of matching nodes.
+    """
+    # This a generator whose elements are, for each sentence, a list
+    # of the matching nodes.
+    gen = tgrep.tgrep_nodes(query, forest)
+    return sum(len(nodes) for nodes in gen)
+
+
+def main() -> None:
+    # Loads collection of trees.
+    forest = []
+    with open(DATA, "r") as source:
+        for line in source:
+            forest.append(tree.ParentedTree.fromstring(line.rstrip()))
+    # Runs queries.
+    print(f"1:\t{count_nodes('FRAG', forest):>5,}")
+    print(f"2:\t{count_nodes('CD', forest):>5,}")
+    print(f"3:\t{count_nodes('NP < DT', forest):>5,}")
+    print(f"4:\t{count_nodes('NP < (DT < a)', forest):>5,}")
+    print(f"5:\t{count_nodes('NP < (NP $ CC $ NP)', forest):>5,}")
+    print(f"6.\t{count_nodes('VP << NP', forest):>5,}")
+    print(f"7.\t{count_nodes('VP < NP', forest):>5,}")
+    print(f"8.\t{count_nodes('VP !< NP', forest):>5,}")
+    print(f"9.\t{count_nodes('VP < (NP $ NP)', forest):>5,}")
+    print(f"10.\t{count_nodes('VP < (NP < PP)', forest):>5,}")
+
+
+if __name__ == "__main__":
+    main()
